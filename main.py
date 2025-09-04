682914d4325950f9873e5608cd20350fa66d9ab9
TexZ-GenZ <priyanshusamal21@gmail.com>
2025-09-04 07:33:43 +0000

    sys argv

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import sys
 from stats import count_words, char_count , make_list , sort_list
 
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 def get_book_text(path_to_file):
     try:
         with open(path_to_file) as f:
@@ -14,7 +18,7 @@ def print_list(char_list):
         print(f"{item['char']}: {item['count']}")
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
 
     total_words = count_words(book_text)
